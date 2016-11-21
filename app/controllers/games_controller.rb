3ea301543ceb5147d606f18e67d41292d30d46fc
Julie Pierre <julie.pierre3@gmail.com>
2016-11-21 18:23:57 +0100

    started the index method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,15 @@
 class GamesController < ApplicationController
+  def index
+    if params[:search]
+      if params[:players]
+        @games = Game.where("min_players > ? AND max_players < ?", params[:players], params[:players]).where("name LIKE ? OR address LIKE ?", params[:search], "%#{params[:search]}%")
+      else
+        @games = Game.where("name LIKE ? OR address LIKE ?", params[:search], "%#{params[:search]}%")
+      end
+    elsif params[:players]
+      @games = Game.where("min_players > ? AND max_players < ?", params[:players], params[:players])
+    else
+      @games = Game.all
+   end
+  end
 end
