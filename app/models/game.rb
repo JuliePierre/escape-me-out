f19cb62e92bc77dd09a874270571e747b60011e3
Julie Pierre <julie.pierre3@gmail.com>
2016-11-21 15:53:11 +0100

    first_steps

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :min_players, presence: true, numericality: { only_integer: true }
+  validates :max_players, presence: true, numericality: { only_integer: true }
+  validates :price_per_hour, presence: true
 end
