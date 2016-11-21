710fcce971bf8724023605425e24625872d77963
Jeremy ISRAEL <israel.jeremy@gmail.com>
2016-11-21 18:57:27 +0100

    beginning of game show and game#availabilities

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+  def show
+    @game = Game.find(params[:id])
+    @booking = Booking.new
+    @date = get_date
+  end
+
+  private
+
+  def get_date
+    Date.today
+  end
 end
