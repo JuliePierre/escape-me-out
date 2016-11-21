c018281eaff152fc5e0b9e069c3c0c88613fcbc9
Julie Pierre <julie.pierre3@gmail.com>
2016-11-21 15:56:52 +0100

    added booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :nb_players, presence: true, numericality: { only_integer: true }
+  validates :starts_at, presence: true
+  validates :duration, presence: true
 end
