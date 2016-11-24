d81d214eb1cd2cc10b642c8aa1e1e85527829a41
Jerome BURKARD <jerome.burkard@gadz.org>
2016-11-24 19:31:05 +0100

    jquery pour hover card sur map debut

---
+++ app/assets/javascripts/maps_hover.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  $('.card').on('mouseover', function(event) {
+    console.log('OK go to next step');
+  });
+
+
+  // marker.addListener('mouseover', function() {
+  //     infowindow.open(map, this);
+  // });
+
+  //   // assuming you also want to hide the infowindow when user mouses-out
+  // marker.addListener('mouseout', function() {
+  //       infowindow.close();
+  // });
+
+});
+
+//  var ourMarker = getMarker(title)
+//  google.maps.event.trigger(ourMarker, 'click');
+
+// function MarkerZoomTo(markerIdentifier) {
+//     pt = gMarkers[markerIdentifier].getPosition();
+//     newpt = new google.maps.LatLng(pt.lat() + .02, pt.lng());
+//     map.panTo(newpt);
+
+//     if (infoWindow) {
+//     infoWindow.close();
+//     }
+
+//     infoWindow.setContent(gMarkers[markerIdentifier].get('iwcontent'));
+//     infoWindow.setPosition(gMarkers[markerIdentifier].getPosition());
+
+//     infoWindow.open(map, gMarkers[markerIdentifier]);
+// }
